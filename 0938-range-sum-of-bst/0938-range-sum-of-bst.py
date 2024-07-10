88b789b13e8cc90a0e85070827de1baa8117cedd
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-07-09 22:29:42 -0700

    Time: 170 ms (68.93%), Space: 27.9 MB (32.16%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.py
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.py
@@ -15,13 +15,28 @@ class Solution(object):
         :type high: int
         :rtype: int
         """
+        
+        # Optimized Solution
+        def helper1(node, l, r):
+            if not node:
+                return 0
+            if node.val < l:
+                return helper1(node.right, l, r)
+            if node.val > r:
+                return helper1(node.left, l , r)
+            return helper1(node.left, l, node.val) + node.val + helper1(node.right, node.val, r)
+        
+        return helper1(root, low, high)
+        
+        # Basic PreOrder Solution
         def helper(node):
             if not node:
                 return None
             if low <= node.val <= high:
                 self.total = self.total + node.val
-            helper(node.left)
+            
             helper(node.right)
+            helper(node.left)
         
         helper(root)
         return self.total
