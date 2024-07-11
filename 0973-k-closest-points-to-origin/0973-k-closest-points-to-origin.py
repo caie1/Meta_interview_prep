df5e839a0152a44a79b4073b9e126ea4ab7fe6a9
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-07-11 00:03:09 -0700

    Time: 610 ms (36.52%), Space: 23.1 MB (28.58%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,19 +1,84 @@
-class Solution(object):
-    def kClosest(self, points, k):
+class Solution:
+    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
         """
-        :type points: List[List[int]]
-        :type k: int
-        :rtype: List[List[int]]
+        Use euclidean distance to find closest points to origin.
+        Heap to store euclidean distance along with points
+        Pop elements from heap until size of the heap is K
+        Finally return points from heap
+        N -> No. of points
+        Time O(2N + KlogN + K) Space O(N)
         """
-        res=[]
-        if k==0:
-            return res
-        heap=[]
-        for i,l in enumerate(points):
-            p=(l[0]**2)+(l[1]**2)
-            heapq.heappush(heap,[p,l[0],l[1]])
-        while k>0:
-            p,x,y=heapq.heappop(heap)
-            res.append([x,y])
-            k-=1
+        ##Erase this solution
+        '''Build max heap => pq = [2, 4] and dist 3 in this scenario, we need to pop 4'''
+        pq = []
+        for x, y in points:
+            dist = x**2 + y**2
+            if len(pq) < K:
+                heapq.heappush(pq, (-dist, [x, y]))
+            elif dist < -pq[0][0]:
+                heapq.heappop(pq)
+                heapq.heappush(pq, (-dist, [x, y]))
+        result = []
+        while pq:
+            result.append(heapq.heappop(pq)[1])
+        return result
+                
+
+        
+
+
+
+
+
+        h = []
+        for x, y in points:
+            dist = x**2 + y**2
+            heapq.heappush(h, (dist, [x, y]))
+        result = []
+        for _ in range(K):
+            result.append(heapq.heappop(h)[1])
+        return result
+
+
+
+
+
+
+        p = []
+        for x, y in points: ##O(N)
+            heapq.heappush(p, (x**2 + y**2, [x, y])) ##O(logN)
+        res = []
+        for i in range(K): ##O(K)
+            res.append(heapq.heappop(p)[1]) ##O(logK)
         return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         p = []
+        
+#         for point in points:  ##O(n)
+#             val = point[0]**2 + point[1]**2
+#             p.append((-val, point))  ###IMP --> max heap 
+        
+#         heapq.heapify(p) ##O(n)
+        
+#         while len(p) > K: ##O(K)
+#             heapq.heappop(p)  ##O(logn)
+        
+#         return [point[1] for point in p] ##O(k)
+    
+        # points.sort(key=lambda x: x[0]**2 + x[1]**2)
+        # return points[:K]
+    #Time : O(NlogN)
+    #Space : O(N)
