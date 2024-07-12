2f87bec9fecdaae9dfd3741a88510d7eb70577c9
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-07-12 16:54:02 -0700

    Time: 276 ms (56.78%), Space: 11.7 MB (98.31%) - LeetHub

--- 1216-valid-palindrome-iii/1216-valid-palindrome-iii.py
+++ 1216-valid-palindrome-iii/1216-valid-palindrome-iii.py
@@ -5,6 +5,29 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
+        n=len(s)
+        t=s[::-1]
+
+        #dp=[[0]*(n+1) for _ in xrange(n+1)]
+
+        prev=[0]*(n+1)
+
+        for i in xrange(1,n+1):
+            cur=[0]*(n+1)
+            for j in xrange(1,n+1):
+                if s[i-1]==t[j-1]:
+                    cur[j]=1+prev[j-1]
+                else:
+                    cur[j]=max(prev[j],cur[j-1])
+            prev=cur
+        return n - prev[n] <= k                   
+        
+        
+        
+        
+        
+        
+        
         l, r = 0, len(s) - 1
         memo = {}
         def helper(l, r, k):
