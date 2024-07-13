48ad3231088fa0bfbfc372e66adfe68399420e25
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-07-13 16:41:45 -0700

    Time: 74 ms (60.97%), Space: 19.2 MB (20.22%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -5,6 +5,19 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
+        hash1={}
+        res=[]
+        freq=[[] for i in range(len(nums)+1)]
+        for n in nums:
+            hash1[n]=1+hash1.get(n,0)
+        for c,v in hash1.items():
+            freq[v].append(c)
+        for i in range(len(freq)-1,0,-1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res)==k:
+                    return res
+        
         '''
         Store frequency in dictionary. Make a use of min heap of size k. Traverse on dictionary, if current freq of num > than lowest freq of num in heap then perform heappop and push current num with its freq to heap.
         At any point of time, heap will contain k maximum element
