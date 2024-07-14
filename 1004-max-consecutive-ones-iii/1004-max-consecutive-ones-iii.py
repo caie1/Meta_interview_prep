227240d9b44966be840b5c9dc3feb2e0e314e71f
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-07-14 15:54:30 -0700

    Time: 386 ms (100.00%), Space: 12.7 MB (9.66%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -5,6 +5,20 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        ##Time: O(N) Space: O(1)
+        l = 0
+        for r in range(len(nums)):
+            if nums[r] == 0:
+                k -= 1
+            if k < 0:
+                if nums[l] == 0:
+                    k += 1
+                l += 1
+        # return r - l + 1
+        return len(nums) - l
+   
+        
+        
         countZero = 0
         maxLen = 0
         
