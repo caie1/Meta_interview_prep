c6492786d79406ad7567386ba6bbce40bfe3ba88
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-07-14 21:59:29 -0700

    Time: 15 ms (81.77%), Space: 11.9 MB (8.29%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -11,7 +11,7 @@ class Solution(object):
             for c1, c2 in zip(w1, w2):
                 if charMap[c1] != charMap[c2]:
                     return charMap[c1] < charMap[c2]
-            return len(w1) <= len(w2)
+            return len(w1) <= len(w2) # This will only execute if all the characters matches
         
         for i in range(len(words) - 1):
             if not is_sorted(words[i], words[i + 1]):
