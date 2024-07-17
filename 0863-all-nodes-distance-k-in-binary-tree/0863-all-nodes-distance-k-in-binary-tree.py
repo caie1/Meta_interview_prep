b32fd9298e995d572cebb56666494c0ed2f85db4
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-07-16 23:59:29 -0700

    Time: 21 ms (44.57%), Space: 12 MB (66.67%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -20,13 +20,48 @@ class Solution(object):
                 parentMap[node.right] = node
                 q.append(node.right)        
         
-    def distanceK(self, root, target, k):
+    def distanceK(self, root, target, K):
         """
         :type root: TreeNode
         :type target: TreeNode
         :type k: int
         :rtype: List[int]
         """
+        """
+        1. Build parent dictionary
+        2. Use deque and push target node with distance 0
+        3. Start performing BFS from target node 
+        Time O(N) Space O(N)
+        """
+        # self.root, self.target = root, target
+        # self._K = K
+        # self._preorder(root, None)
+        # return self._BFS()
+        
+        parent_map = {}
+        def _preorder(node, parent):
+            if node:
+                parent_map[node] = parent
+                _preorder(node.left, node)
+                _preorder(node.right, node)
+        _preorder(root, None)
+        if target not in parent_map:
+            return []
+        ## BFS
+        dq = deque([(target, 0)])
+        visited = {target}
+        while dq:
+            node, dist = dq.popleft()
+            if dist == K:
+                return [node.val] + [n.val for n, d in dq] 
+            for nei in [node.left, node.right, parent_map[node]]:
+                if nei and nei not in visited:
+                    dq.append((nei, dist + 1))
+                    visited.add(nei)
+        return []
+        
+        
+        
         parentMap = {}
         self.adjecency(root,parentMap)
         
