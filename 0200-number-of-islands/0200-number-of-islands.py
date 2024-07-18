6bf737214118e074eb7085c47af6577ca67f8aca
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-07-17 17:53:50 -0700

    Time: 237 ms (47.91%), Space: 30.7 MB (26.68%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -5,6 +5,17 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
+        def bfs(i, j):
+            dq = deque([(i, j)])
+            while dq:
+                i, j = dq.popleft()
+                for x, y in directions:
+                    newx, newy = i + x, j + y
+                    if 0 <= newx < rows and 0 <= newy < cols and grid[newx][newy] == '1' and (newx, newy) not in visit:
+                        visit.add((newx, newy))
+                        dq.append((newx, newy))
+
+        
         def dfs(r,c, visit, directions):
             visit.add((r,c))
             
@@ -14,14 +25,14 @@ class Solution(object):
                 if 0 <= row < len(grid) and 0 <= col < len(grid[0]) and grid[row][col] == "1" and (row, col) not in visit:
                     dfs(row, col, visit, directions)
             
-            
+        rows, cols = len(grid), len(grid[0])    
         directions = [[-1,0], [1,0], [0,-1], [0,1]]
         visit = set()
         count = 0
-        for i in xrange(len(grid)):
-            for j in xrange(len(grid[0])):
+        for i in xrange(rows):
+            for j in xrange(cols):
                 if grid[i][j] == "1" and (i, j) not in visit:
-                    dfs(i,j,visit,directions)
+                    bfs(i,j)
                     count += 1
         
         return count
