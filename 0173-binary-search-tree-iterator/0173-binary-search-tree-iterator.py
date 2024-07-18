7d2046d6e632cf7a3833b8b9606a80f35471d8c4
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-07-17 20:58:17 -0700

    Time: 48 ms (73.33%), Space: 20.2 MB (45.96%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
@@ -10,37 +10,66 @@ class BSTIterator(object):
         """
         :type root: TreeNode
         """
-        self.res = []
-        
-        self.helper(root, self.res)
-        
-        self.pointer = - 1
-        self.length = len(self.res)
-        
-        
-    def helper(self, node, arr):
-        if not node:
-            return None
-        
-        self.helper(node.left, arr)
-        arr.append(node.val)
-        self.helper(node.right, arr)
+        self.stack = []
+        self.extract(root)
+    
+    def extract(self, node):
+        while node:
+            self.stack.append(node)
+            node = node.left        
         
     def next(self):
         """
         :rtype: int
         """
-        self.pointer += 1
-        if self.pointer < self.length:
-            return self.res[self.pointer]
-
+        node = self.stack.pop() # Next is valid call as per the question requirements
+        
+        if node.right:
+            self.extract(node.right)
+        return node.val
+        
     def hasNext(self):
         """
         :rtype: bool
         """
-        if self.pointer == self.length - 1:
-            return False
-        return True
+        return len(self.stack) > 0
+
+    
+#     def __init__(self, root):
+#         """
+#         :type root: TreeNode
+#         """
+#         self.res = []
+        
+#         self.helper(root, self.res)
+        
+#         self.pointer = - 1
+#         self.length = len(self.res)
+        
+        
+#     def helper(self, node, arr):
+#         if not node:
+#             return None
+        
+#         self.helper(node.left, arr)
+#         arr.append(node.val)
+#         self.helper(node.right, arr)
+        
+#     def next(self):
+#         """
+#         :rtype: int
+#         """
+#         self.pointer += 1
+#         if self.pointer < self.length:
+#             return self.res[self.pointer]
+
+#     def hasNext(self):
+#         """
+#         :rtype: bool
+#         """
+#         if self.pointer == self.length - 1:
+#             return False
+#         return True
 
 
 # Your BSTIterator object will be instantiated and called as such:
