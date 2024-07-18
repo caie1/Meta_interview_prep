23bf902b4a5facfd2a1de762d74010e246ef47bb
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-07-17 18:12:49 -0700

    Time: 118 ms (51.76%), Space: 12.9 MB (23.27%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,6 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+        pos = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[pos], nums[i] = nums[i], nums[pos]
+                pos += 1
+        
+        
+        
+        
         l = 0 
         while l < len(nums) and nums[l] != 0:
             l += 1
