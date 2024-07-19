cc9135f14b4bc587534f2f8198a7cc10066dbd40
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-07-19 15:39:35 -0700

    Time: 46 ms (53.50%), Space: 15.2 MB (16.00%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.py
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.py
@@ -4,6 +4,28 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: int
         """
+        '''
+        Once we found the top-left battelship, we found a battleship. 'X' those are not located top-left of the contagious 'X'
+        will not add up to the number of battleship. 
+        1. Battleships are not contaigous to each other. 
+        2. Battleship are of shape 1*n or n *1        
+        '''
+        rows, cols = len(board), len(board[0])
+        count = 0
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == '.':
+                    continue
+                if i > 0 and board[i - 1][j] == 'X':
+                    continue
+                if j > 0 and board[i][j - 1] == 'X':
+                    continue
+                count += 1
+        return count
+        
+        
+        
+        
         m = len(board)
         n = len(board[0])
         
