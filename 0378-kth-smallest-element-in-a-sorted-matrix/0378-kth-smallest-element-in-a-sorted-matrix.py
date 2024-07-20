05b862eee60ede1c22c0a731f992a616c7a27db5
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-07-19 17:18:46 -0700

    Time: 125 ms (76.98%), Space: 15.8 MB (32.33%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -5,6 +5,34 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        # Binary Search Optimization
+        def getRank(num):
+            row, col = len(matrix) - 1, 0
+            rank = 0
+            
+            while row >= 0 and col < len(matrix[0]):
+                
+                if matrix[row][col] <= num:
+                    rank += row + 1
+                    col += 1
+                else:
+                    row -= 1
+            return rank
+        
+        
+        l, r = matrix[0][0], matrix[-1][-1]
+        
+        while l <= r:
+            mid = (l + r) // 2
+            
+            if getRank(mid) < k:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return l
+        
+        
+        
         # Heap solution T: O(n2logk) S:O(k)
         heap = []
         for i in xrange(len(matrix)):
