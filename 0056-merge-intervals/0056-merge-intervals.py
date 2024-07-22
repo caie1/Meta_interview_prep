29fba03586c61685424af8a1ead6f60c428f0ed8
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-07-21 20:07:15 -0700

    Time: 108 ms (25.21%), Space: 16.7 MB (64.78%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -4,29 +4,13 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: List[List[int]]
         """
-        intervals.sort(key=lambda x:x[0])
-        result = []
-        for i in range(len(intervals)):
-            if not result or result[-1][1] < intervals[i][0]:
-                result.append(intervals[i])
-            else:
-                result[-1][1] = max(result[-1][1], intervals[i][1])
-        return result      
-        
-        
-        
-        
-        
+        intervals.sort(key = lambda x : x[0])
         res = []
-        intervals.sort(key = lambda x:x[0])
-        i = 0
         
-        while i < len(intervals) - 1:
-            if intervals[i][1] < intervals[i+1][0]: # If [1,3],[4,6] then no issue
+        for i in xrange(len(intervals)):
+            if not res or (res[-1][1] < intervals[i][0]):
                 res.append(intervals[i])
-            else: # else we modify the next interval
-                intervals[i+1][0] = min(intervals[i][0], intervals[i+1][0])
-                intervals[i+1][1] = max(intervals[i][1], intervals[i+1][1])
-            i += 1
-        res.append(intervals[i])
+            else:
+                res[-1][0] = min(res[-1][0], intervals[i][0])
+                res[-1][1] = max(res[-1][1], intervals[i][1])
         return res
