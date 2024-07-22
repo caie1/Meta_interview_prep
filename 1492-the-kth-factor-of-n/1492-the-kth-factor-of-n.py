3fe29120382bb26671411c1a176d55ba68f4d7ce
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-07-21 23:32:59 -0700

    Time: 25 ms (7.09%), Space: 11.8 MB (23.74%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
@@ -1,16 +1,43 @@
+import math
+
 class Solution(object):
+    
+    def __init__(self):
+        self.k = 0
+        self.heap = []
+        
+    def kHeap(self, num):
+        heapq.heappush(self.heap, - num) # Max heap structure
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+            
     def kthFactor(self, n, k):
         """
         :type n: int
         :type k: int
         :rtype: int
         """
-        # Brute Force Solution
-        count = 1
+        # Optimal Solution
+        """
+        While figuring divisors, we have some small divisor multplied with big divisor so only scan till 
+        sqrt(n) + 1
+        """
+        self.k = k
         
-        for num in xrange(1, n + 1):
+        for num in xrange(1, int(math.sqrt(n)) + 1):
             if n % num == 0:
-                if count == k:
-                    return num
-                count += 1
-        return - 1
+                self.kHeap(num)
+                if num != (n // num):
+                    self.kHeap(n // num)
+                    
+        return - self.heap[0] if len(self.heap) == self.k else -1
+        
+        # Brute Force Solution
+#         count = 1
+        
+#         for num in xrange(1, n + 1):
+#             if n % num == 0:
+#                 if count == k:
+#                     return num
+#                 count += 1
+#         return - 1
