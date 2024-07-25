e0378a95310a5f399d55888026a430d5453c6f1d
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-07-24 18:35:27 -0700

    Time: 47 ms (97.22%), Space: 13.3 MB (42.52%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -4,6 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        pos = 0
+        for j in range(1, len(nums)):
+            if nums[pos] != nums[j]:
+                pos += 1
+                nums[pos] = nums[j]
+        return pos + 1
+    
+    
         l = r = 0
         while r < len(nums):
             while (r < len(nums)) and nums[r] == nums[l]:
