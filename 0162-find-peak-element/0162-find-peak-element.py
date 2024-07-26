c25549977c7c0b27a61fd864e6f7f6b4b2c5108c
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-07-26 15:28:43 -0700

    Time: 31 ms (21.04%), Space: 11.5 MB (98.74%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -4,20 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums) == 1 or nums[0] > nums[1]:
+        if len(nums) == 1:
             return 0
-        if nums[-1] > nums[-2]:
+        if nums[0] > nums[1]:
+            return 0
+        elif nums[-1] > nums[-2]:
             return len(nums) - 1
-            
-        l = 1 # l should start from 1 because we are comparing nums[mid - 1]
-        r = len(nums) - 2 # r ends at n-2 because we are comparing nums[mid + 1]
+        
+        l, r = 1, len(nums) - 2
         
         while l <= r:
             mid = (l + r) // 2
             
-            if nums[mid - 1] < nums[mid] > nums[mid + 1]:
+            if nums[mid - 1] < nums[mid] and nums[mid] > nums[mid + 1]:
                 return mid
-            elif nums[mid - 1] < nums[mid] < nums[mid + 1]: # We are in increasing sequence
+            elif nums[mid -  1] < nums[mid] < nums[mid + 1]:
                 l = mid + 1
-            else: # We are in decreasing sequence
+            else:
                 r = mid - 1
